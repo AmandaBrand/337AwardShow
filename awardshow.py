9731c683f54d984e9000d7f1f588c0a5ee7ac68f
William Gross <grossbill6@gmail.com>
2014-02-09 18:08:46 -0600

    person_search
    A first try at pulling people's names from tweets

--- awardshow.py
+++ awardshow.py
@@ -1,10 +1,40 @@
 import nltk, json
 
-with open('goldenglobes.json', 'r') as f:
+with open('/Users/williamgross/337AwardShow/goldenglobes.json', 'r') as f:
      tweets = map(json.loads, f)
 
 tweet_txt_array = []
 for tweet in tweets:
 	tweet_txt_array.append(tweet['text'])
 
-print tweet_txt_array
+## Currently, I can only get the functions to work by copying these definitions into
+## the command line and running them there 
+def person_search(text):
+	"Takes a piece of text, pulling out people names"
+	import nltk
+	sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+	sentences = sent_detector.tokenize(text)
+	tokens = []
+	tags = []
+	entities = []
+	for sentence in sentences:
+		temp = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sentence)))
+		for entity in temp:
+			try:
+				nodeLabel = entity.node;
+			except AttributeError:
+				pass
+			else:
+				if (nodeLabel=='PERSON' and len(entity)==2):
+					person = entity[0][0]+" "+entity[1][0]
+					entities.append(person)
+	return entities
+
+def looped_search(tweets):
+	"Loops through tweets and exports people found in them as a list"
+	people = [];
+	for tweet in tweets:
+		temp = person_search(tweet['text'])
+		if temp!=[]:
+			people.append(temp)
+	return people
