65a5061693f81ba2d7cc772865ee5ef249d9955b
Rich Chang <changaboy24@gmail.com>
2014-02-14 02:14:58 -0600

    host names, added tarantino to stop list, run 'python awardshow'

--- awardshow.py
+++ awardshow.py
@@ -1,5 +1,4 @@
 import nltk, json
-import namescraper
 
 with open('goldenglobes.json', 'r') as f:
      tweets = map(json.loads, f)
@@ -7,18 +6,25 @@ with open('goldenglobes.json', 'r') as f:
 # for tweet in tweets:
 # 	tweet_txt_array.append(tweet['text'])
 
+def get_dict_buckets():
+	text_file = open("dict_buckets.txt", "r")
+	lines = text_file.readlines()
+	buckets = []
+	for line in lines:
+		 buckets.append(line.rstrip())
+	return buckets
+
 ## Currently, I can only get the functions to work by copying these definitions into
 ## the command line and running them there 
 def person_search(text):
 	"Takes a piece of text, pulling out people names"
-	import nltk
 	sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 	namecorpus = nltk.corpus.names
 	names = namecorpus.words('male.txt')
 	names.extend(namecorpus.words('female.txt'))
 	sentences = sent_detector.tokenize(text)
 	entities = []
-	stoplist = ["Justin REALLY", "Clinton Endorses"]
+	stoplist = ["Justin REALLY", "Clinton Endorses", "Quentin Tarantino"]
 	for sentence in sentences:
 		temp = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sentence)))
 		for entity in temp:
@@ -35,26 +41,49 @@ def person_search(text):
 						entities.append(person)
 	return entities
 
-
-def looped_search(tweets):
-	"Loops through tweets and exports people found in them as a list"
-	award_names = namescraper.get_names()
-	#makes a list out of award names in awardnames.txt
-
-	# people = [];
+#makes a dictionary with buckets dictated in dict_buckets.txt
+def make_tweet_dict():
+	dict_buckets = get_dict_buckets()
 	tweets_dict = {}
 	for tweet in tweets:
-		for award in award_names:
+		for bucket in dict_buckets:
 			tweet_text = tweet['text']
-			if award in tweet_text:
-				if award in tweets_dict.keys():
-					tweets_dict[award].append(tweet_text)
+			if bucket in tweet_text:
+				if bucket in tweets_dict.keys():
+					tweets_dict[bucket].append(tweet_text)
 				else:
-					#if a tweet for that award hasn't been stored yet, start the array
-					tweets_dict[award] = [tweet_text]
-
-		# temp = person_search(tweet['text'])
-		# if temp!=[]:
-		# 	people.extend(temp)
+					#if a tweet for that bucket hasn't been stored yet, start the array
+					tweets_dict[bucket] = [tweet_text]
 	return tweets_dict
+
+def find_hosts(host_tweets):
+	name_array = []
+	count_array = []
+	for tweet in host_tweets:
+		people = person_search(tweet)
+		if len(people):
+			for person in people:
+				if person in name_array:
+					index = name_array.index(person)
+					count_array[index] = count_array[index] + 1
+				else:
+					name_array.append(person)
+					count_array.append(1)
+	##get top 2
+	sorted_counts = sorted(count_array)
+	most = sorted_counts[-1]
+	second_most = sorted_counts[-2]
+	most_index = count_array.index(most)
+	second_index = count_array.index(second_most)
+
+	return name_array[most_index], name_array[second_index]
+
+def main():
+	tweet_dict = make_tweet_dict()
+
+	hosts = find_hosts(tweet_dict['Hosts'])
+
+	print hosts
+	return hosts
 	# return people
+main()
--- namescraper.py
+++
@@ -1,9 +0,0 @@
-def get_names():
-	text_file = open("awardnames.txt", "r")
-	lines = text_file.readlines()
-	array = []
-	for line in lines:
-		 array.append(line.rstrip())
-	return array
-
-get_names()
