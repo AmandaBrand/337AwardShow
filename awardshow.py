3cbae67c59825159fcc12ba20f26e8221947d505
Rich Chang <changaboy24@gmail.com>
2014-02-14 11:46:01 -0600

    works oddly well

--- awardshow.py
+++ awardshow.py
@@ -5,85 +5,36 @@ with open('goldenglobes.json', 'r') as f:
 # tweet_txt_array = []
 # for tweet in tweets:
 # 	tweet_txt_array.append(tweet['text'])
-
-def get_dict_buckets():
-	text_file = open("dict_buckets.txt", "r")
-	lines = text_file.readlines()
-	buckets = []
-	for line in lines:
-		 buckets.append(line.rstrip())
-	return buckets
-
-## Currently, I can only get the functions to work by copying these definitions into
-## the command line and running them there 
-def person_search(text):
-	"Takes a piece of text, pulling out people names"
-	sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-	namecorpus = nltk.corpus.names
-	names = namecorpus.words('male.txt')
-	names.extend(namecorpus.words('female.txt'))
-	sentences = sent_detector.tokenize(text)
-	entities = []
-	stoplist = ["Justin REALLY", "Clinton Endorses", "Quentin Tarantino"]
-	for sentence in sentences:
-		temp = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sentence)))
-		for entity in temp:
-			try:
-				nodeLabel = entity.node;
-			except AttributeError:
-				pass
-			else:
-				if (nodeLabel=='PERSON' and len(entity)==2 and entity[0][0] in names):
-					person = entity[0][0]+" "+entity[1][0]
-					if (person in stoplist):
-						pass
-					else:
-						entities.append(person)
-	return entities
-
-#makes a dictionary with buckets dictated in dict_buckets.txt
-def make_tweet_dict():
-	dict_buckets = get_dict_buckets()
-	tweets_dict = {}
+def clean(tweet):
+	tweet = tweet.replace("RT @goldenglobes: ","")
+	tweet = tweet.replace("- #GoldenG","").replace("- #GoldenGlobes","").replace("lobes","")
+	return tweet
+def loop():
+	already_seen = []
 	for tweet in tweets:
-		for bucket in dict_buckets:
-			tweet_text = tweet['text']
-			if bucket in tweet_text:
-				if bucket in tweets_dict.keys():
-					tweets_dict[bucket].append(tweet_text)
-				else:
-					#if a tweet for that bucket hasn't been stored yet, start the array
-					tweets_dict[bucket] = [tweet_text]
-	return tweets_dict
+		temp_array = []
+		tweet_text = tweet["text"]
+		if "RT @goldenglobes: Best" in tweet_text:
+			tweet_text = clean(tweet_text)
+			if "RT" not in tweet_text:##more cleaning
+				temp_array = tweet_text.split("-")
 
-def find_hosts(host_tweets):
-	name_array = []
-	count_array = []
-	for tweet in host_tweets:
-		people = person_search(tweet)
-		if len(people):
-			for person in people:
-				if person in name_array:
-					index = name_array.index(person)
-					count_array[index] = count_array[index] + 1
-				else:
-					name_array.append(person)
-					count_array.append(1)
-	##get top 2
-	sorted_counts = sorted(count_array)
-	most = sorted_counts[-1]
-	second_most = sorted_counts[-2]
-	most_index = count_array.index(most)
-	second_index = count_array.index(second_most)
+				#second part of this if is to get out a lot of RT's of "goldenglobe"
+				if hash(tweet_text) not in already_seen and "Best" == temp_array[0][:4]:
 
-	return name_array[most_index], name_array[second_index]
+					already_seen.append(hash(tweet_text))
+					temp_array = tweet_text.split("-")
+					length = len(temp_array)
+					if length == 3:
+						#[award, winner, movie]
+						print temp_array[1] + "wins " +temp_array[0] + "in " +temp_array[2]
+					if length == 4:
+						#[award, category, winer, movie ]
+						print temp_array[2]+"wins "+temp_array[0]+ "-" +temp_array[1]+ "in "+temp_array[3]
+					if length == 2:
+						#[award, winner]
+						print temp_array[1] + "wins "+temp_array[0]
+					print ""
 
-def main():
-	tweet_dict = make_tweet_dict()
+loop()
 
-	hosts = find_hosts(tweet_dict['Hosts'])
-
-	print hosts
-	return hosts
-	# return people
-main()
