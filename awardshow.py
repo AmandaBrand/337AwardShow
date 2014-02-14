3829e51b5413ee415037cc0360b00ca031caf6cb
Rich Chang <changaboy24@gmail.com>
2014-02-14 11:59:59 -0600

    added comments

--- awardshow.py
+++ awardshow.py
@@ -10,21 +10,26 @@ def clean(tweet):
 	tweet = tweet.replace("- #GoldenG","").replace("- #GoldenGlobes","").replace("lobes","")
 	return tweet
 def loop():
-	already_seen = []
+	already_seen = [] #will hold hashes of tweets so we don't print the same tweet twice
+
 	for tweet in tweets:
-		temp_array = []
+		temp_array = [] #holds split tweet (i.e. ["Best Actor", "Russell Crowe", "Les Miserable"])
 		tweet_text = tweet["text"]
+
 		if "RT @goldenglobes: Best" in tweet_text:
 			tweet_text = clean(tweet_text)
-			if "RT" not in tweet_text:##more cleaning
-				temp_array = tweet_text.split("-")
 
-				#second part of this if is to get out a lot of RT's of "goldenglobe"
+			if "RT" not in tweet_text:##more cleaning
+
+				temp_array = tweet_text.split("-")#the tweets look like "Best Actor - Russell Crowe - Les Miserable"
+
 				if hash(tweet_text) not in already_seen and "Best" == temp_array[0][:4]:
+				#first part, checking if we have seen it before
+				#second part is just cleaning out tweets that don't start with "Best"
 
 					already_seen.append(hash(tweet_text))
-					temp_array = tweet_text.split("-")
 					length = len(temp_array)
+					#different formats of tweets based on how many "-" are included in the tweet
 					if length == 3:
 						#[award, winner, movie]
 						print temp_array[1] + "wins " +temp_array[0] + "in " +temp_array[2]
